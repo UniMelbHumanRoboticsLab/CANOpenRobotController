daa2e1077e02a2afbefb52c493de7c255177fd3c
Willcampbellmelb <w.campbell@student.unimelb.edu.au>
2020-05-12 19:04:12 +1000

    Update RobotParams.h

--- src/apps/RobotParams.h
+++ src/apps/RobotParams.h
@@ -1,5 +1,5 @@
 /**
- * Specific paramaters and naming definitions for exoskeleton robot class implementetion.
+ * \brief pecific paramaters and naming definitions for exoskeleton robot class implementetion.
  * 
  * Version 0.1
  * Date: 07/04/2020
@@ -18,16 +18,22 @@
 #define deg2rad(deg) ((deg)*M_PI / 180.0)
 #define rad2deg(rad) ((rad)*180.0 / M_PI)
 
-//Joint Index for the 6 joints (note, NODEID = this + 1)
+/**
+ * An enum type.
+ * Joint Index for the 6 joints (note, NODEID = this + 1)
+ */
 enum robotJoints {
-    LEFT_HIP = 0,
-    LEFT_KNEE = 1,
-    RIGHT_HIP = 2,
-    RIGHT_KNEE = 3,
-    LEFT_ANKLE = 4,
-    RIGHT_ANKLE = 5
+    LEFT_HIP = 0,   /**< Left Hip*/
+    LEFT_KNEE = 1,  /**< Left Knee*/
+    RIGHT_HIP = 2,  /**< Right Hip*/
+    RIGHT_KNEE = 3, /**< Right Knee*/
+    LEFT_ANKLE = 4, /**< Left Ankle*/
+    RIGHT_ANKLE = 5 /**< Right Knee*/
 };
-
+/**
+ * An enum class.
+ * Different Robot motion profiles
+ */
 enum class RobotMode {
     NORMALWALK,
     SITDWN,
@@ -41,19 +47,26 @@ enum class RobotMode {
     UNEVEN,
     INITIAL
 };
-
-//Params for specific robt
-//Knee motor reading and corresponding angle. Used for mapping between degree and motor values.
+/**
+ * 
+ * Paramater definitions: Knee motor reading and corresponding angle. Used for mapping between degree and motor values.
+ */
 #define KNEE_MOTOR_POS1 (250880)
 #define KNEE_MOTOR_DEG1 (90)
 #define KNEE_MOTOR_POS2 (0)
 #define KNEE_MOTOR_DEG2 (0)
-//Hip motor reading and corresponding angle. Used for mapping between degree and motor values.
+/**
+ * 
+ * Paramater definitions: Hip motor reading and corresponding angle. Used for mapping between degree and motor values.
+ */
 #define HIP_MOTOR_POS1 (250880)
 #define HIP_MOTOR_DEG1 (90)
 #define HIP_MOTOR_POS2 (0)
 #define HIP_MOTOR_DEG2 (180)
-//Ankle motor reading and corresponding angle. Used for mapping between degree and motor values.
+/**
+ * 
+ * Paramater definitions: Ankle motor reading and corresponding angle. Used for mapping between degree and motor values.
+ */
 #define ANKLE_MOTOR_POS1 (0)
 #define ANKLE_MOTOR_DEG1 (90)
 #define ANKLE_MOTOR_POS2 (-800000)
