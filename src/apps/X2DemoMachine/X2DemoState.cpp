8ce1746c39f93a33fbd37a8f37801a87963e3760
emekBaris <baris.kucuktabak@gmail.com>
2020-07-07 09:31:31 -0500

    demo state is created

--- src/apps/X2DemoMachine/X2DemoState.cpp
+++ src/apps/X2DemoMachine/X2DemoState.cpp
@@ -1,5 +1,27 @@
-//
-// Created by emekb on 7/6/20.
-//
-
 #include "X2DemoState.h"
+
+void X2DemoState::entry(void) {
+    std::cout << "Example State Entered " << std::endl
+              << "===================" << std::endl
+              << "===================" << std::endl;
+
+    robot->initVelocityControl();
+    time0 = std::chrono::steady_clock::now();
+}
+void X2DemoState::during(void) {
+
+    float t_final = 5.0;
+    std::vector<double> desiredVelocity(NUM_JOINTS);
+
+    if(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - time0).count() < t_final*1000){
+        desiredVelocity = { 0, 0, 0, M_PI/180.0*(90.0/5.0)} ;
+        robot->setVelocity(desiredVelocity);
+
+    }else{
+        desiredVelocity = { 0, 0, 0, 0} ;
+        robot->setVelocity(desiredVelocity);
+    }
+}
+void X2DemoState::exit(void) {
+    std::cout << "Example State Exited" << std::endl;
+}
--- src/apps/X2DemoMachine/X2DemoState.h
+++ src/apps/X2DemoMachine/X2DemoState.h
@@ -1,14 +1,41 @@
-//
-// Created by emekb on 7/6/20.
-//
-
+/**
+ * /file X2DemoState.h
+ * /author Emek Baris Kucuktabak
+ * /brief Concrete implementation of IdleState
+ * /version 0.1
+ * /date 2020-07-06
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
 #ifndef SRC_X2DEMOSTATE_H
 #define SRC_X2DEMOSTATE_H
 
+#include "State.h"
+#include "ExoRobot.h"
+#include <ctime>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <math.h>
 
-class X2DemoState {
+/**
+ * \brief Demo State for the X2DemoMachine
+ *
+ *
+ */
+class X2DemoState : public State {
+    ExoRobot *robot;
 
+public:
+    void entry(void);
+    void during(void);
+    void exit(void);
+    X2DemoState(StateMachine *m, ExoRobot *exo, const char *name = NULL) : State(m, name), robot(exo){};
+
+private:
+    std::chrono::steady_clock::time_point time0;
 };
 
-
-#endif //SRC_X2DEMOSTATE_H
+#endif
